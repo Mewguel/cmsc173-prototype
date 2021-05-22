6a9eb7e38064c82bbe560acae92df0065e58e1c5
abigailnadua <acnadua@up.edu.ph>
2021-05-22 17:58:16 +0800

    removed grommet, added react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import { theme } from './theme';
-import { Box, Heading, Grommet } from 'grommet';
-import { AppBar } from './components/AppBar';
-import { NavButtonBox } from './components/NavButtonBox';
-import { NavButton } from './components/NavButton';
 import './styles.css';
 
 function App() {
   return (
-    <Grommet theme={theme} full>
-      <Box fill>
-        <AppBar>
-          <NavButtonBox>
-            <Heading level='2' margin='none' className='squada-one'>PANTRY POINT</Heading>
-          </NavButtonBox>
-          <NavButtonBox>
-            <NavButton href=''>MAP</NavButton>
-            <NavButton href=''>REGISTER</NavButton>
-            <NavButton href=''>ABOUT</NavButton>
-          </NavButtonBox>
-        </AppBar>
-        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-          <Box flex align='center' justify='center'>
-            app body
-          </Box>
-        </Box>
-      </Box>
-    </Grommet>
+    <div>
+      Hello World
+    </div>
   );
 }
 
