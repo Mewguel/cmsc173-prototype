a6ba725c8de611f297e1353b7a6c239700d554cc
Bien Custodio <bbcustodio2>
2021-05-26 13:52:05 +0800

    EDIT: fixed code modularity an placed map to Map.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,16 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import { Container } from "react-bootstrap";
-import WrappedMap from "./components/Map"
-import { API_key } from './default.json';
-import PantryInfo from './components/PantryInfo'
+import Map from "./components/Map";
+import PantryInfo from "./components/PantryInfo";
 
 function App() {
-  return (
-    <Container fluid className="containerStyle" >
-      
-      {/*div for the google map Component*/}
-      <div style = {{width: "50vw", height: "50vh"}}>
-      <WrappedMap 
-        isMarkerShown={true}
-        googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_key}`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px`, width: `600px`, margin: `5px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />      
-      </div>
-      <PantryInfo>
-
-      </PantryInfo>
-        
-    </Container>
-  );
+    return (
+        <Container fluid className="containerStyle">
+            <Map></Map>
+            <PantryInfo></PantryInfo>
+        </Container>
+    );
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,47 @@
-import {GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
+import {
+    GoogleMap,
+    withScriptjs,
+    withGoogleMap,
+    Marker,
+} from "react-google-maps";
 import mapStyles from "./Mapstyles";
+import { API_key } from "../default.json";
 
-function Map(props) {
-    return(
+function MapApi(props) {
+    return (
         //set google map coordinates and zoom level
         <GoogleMap
             defaultZoom={18}
             defaultCenter={{ lat: 14.164529, lng: 121.241929 }}
-            defaultOptions={{styles: mapStyles}}
-        >  
-         {props.isMarkerShown && <Marker position={{ lat: 14.164529, lng: 121.241929 }} />}
+            defaultOptions={{ styles: mapStyles }}
+        >
+            {props.isMarkerShown && (
+                <Marker position={{ lat: 14.164529, lng: 121.241929 }} />
+            )}
         </GoogleMap>
     );
-    
-    
 }
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+const WrappedMap = withScriptjs(withGoogleMap(MapApi));
 
-export default WrappedMap;
+function Map() {
+    return (
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <WrappedMap
+                isMarkerShown={true}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_key}`}
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={
+                    <div
+                        style={{
+                            height: `90%`,
+                            width: `75% `,
+                            margin: `5px`,
+                        }}
+                    />
+                }
+                mapElement={<div style={{ height: `100%` }} />}
+            />
+        </div>
+    );
+}
+export default Map;
