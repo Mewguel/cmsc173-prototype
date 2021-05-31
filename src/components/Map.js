adbd3140259070808e41529ea5777a5f45190dcf
Bien Custodio <bbcustodio2>
2021-05-31 18:16:34 +0800

    EDIT: removed markers in google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,80 +6,136 @@ import {
 } from "react-google-maps";
 import { API_key } from "../default.json";
 import mapStyles from "./Mapstyles";
-import PantryInfo from './PantryInfo'
-import { Container} from "react-bootstrap";
-import {Component} from "react";
-class MapApi extends Component{
-    constructor(props){
+import PantryInfo from "./PantryInfo";
+import { Container } from "react-bootstrap";
+import { Component } from "react";
+class MapApi extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-           defaultCenter:{ lat: 14.164529, lng: 121.241929 },
-            markerList:[//LIST OF PANTRIES
-                        {id:0,lat:14.164529,lng:121.241929
-                            ,pantryInfo:{name:"Pantry 731",address:"Batong Malake, Pili Drive",contactNo:"0915-134-0520", gCash:"3213165390",gCashName:"Jimmy Tran",faceBook:"facebook.com/tranJimmy"}
-                        },
-                        {id:1,lat:14.164529,lng:121.24329
-                            ,pantryInfo:{name:"Pantry 24601",address:"Batong Malake, Pili Drive",contactNo:"0915-134-0520", gCash:"3213165390",gCashName:"Bronson Tran",faceBook:"facebook.com/tranBronson"}
-                        },
-                        {id:2,lat:14.165329,lng:121.24229
-                            ,pantryInfo:{name:"Pantry 741",address:"Batong Malake, Pili Drive",contactNo:"0915-134-0520", gCash:"3213165390",gCashName:"Davy Jones",faceBook:"facebook.com/davyjones"}
-
-                        },
-                        {id:3,lat:14.165309,lng:121.24509
-                            ,pantryInfo:{name:"Pantry 666",address:"Batong Malake, Vega Center",contactNo:"0915-134-0520", gCash:"3213165390",gCashName:"Iron Maiden",faceBook:"facebook.com/NumberoftheBeast"}
-
-                        },
-
-                    ],
-            currentPantry:{name:"Pantry 731",address:"Batong Malake, Pili Drive",contactNo:"0915-134-0520", gCash:"3213165390",gCashName:"Jimmy Tran",faceBook:"facebook.com/TJU"}
-        }
-        this.changeDefaultCenter=this.changeDefaultCenter.bind(this);
-        this.changeCurrentPantry=this.changeCurrentPantry.bind(this)
+        this.state = {
+            defaultCenter: { lat: 14.164529, lng: 121.241929 },
+            markerList: [
+                //LIST OF PANTRIES
+                {
+                    id: 0,
+                    lat: 14.164529,
+                    lng: 121.241929,
+                    pantryInfo: {
+                        name: "Pantry 731",
+                        address: "Batong Malake, Pili Drive",
+                        contactNo: "0915-134-0520",
+                        gCash: "3213165390",
+                        gCashName: "Jimmy Tran",
+                        faceBook: "facebook.com/tranJimmy",
+                    },
+                },
+                {
+                    id: 1,
+                    lat: 14.164529,
+                    lng: 121.24329,
+                    pantryInfo: {
+                        name: "Pantry 24601",
+                        address: "Batong Malake, Pili Drive",
+                        contactNo: "0915-134-0520",
+                        gCash: "3213165390",
+                        gCashName: "Bronson Tran",
+                        faceBook: "facebook.com/tranBronson",
+                    },
+                },
+                {
+                    id: 2,
+                    lat: 14.165329,
+                    lng: 121.24229,
+                    pantryInfo: {
+                        name: "Pantry 741",
+                        address: "Batong Malake, Pili Drive",
+                        contactNo: "0915-134-0520",
+                        gCash: "3213165390",
+                        gCashName: "Davy Jones",
+                        faceBook: "facebook.com/davyjones",
+                    },
+                },
+                {
+                    id: 3,
+                    lat: 14.165309,
+                    lng: 121.24509,
+                    pantryInfo: {
+                        name: "Pantry 666",
+                        address: "Batong Malake, Vega Center",
+                        contactNo: "0915-134-0520",
+                        gCash: "3213165390",
+                        gCashName: "Iron Maiden",
+                        faceBook: "facebook.com/NumberoftheBeast",
+                    },
+                },
+            ],
+            currentPantry: {
+                name: "Pantry 731",
+                address: "Batong Malake, Pili Drive",
+                contactNo: "0915-134-0520",
+                gCash: "3213165390",
+                gCashName: "Jimmy Tran",
+                faceBook: "facebook.com/TJU",
+            },
+        };
+        this.changeDefaultCenter = this.changeDefaultCenter.bind(this);
+        this.changeCurrentPantry = this.changeCurrentPantry.bind(this);
     }
 
-    changeDefaultCenter(latitude, longitude){//CHANGE CENTER OF MAP
-        this.setState({...this.state,defaultCenter:{lat:latitude,lng:longitude} });
+    changeDefaultCenter(latitude, longitude) {
+        //CHANGE CENTER OF MAP
+        this.setState({
+            ...this.state,
+            defaultCenter: { lat: latitude, lng: longitude },
+        });
     }
-    changeCurrentPantry(activePantry){//CHANGE CURRENT PANTRY
+    changeCurrentPantry(activePantry) {
+        //CHANGE CURRENT PANTRY
 
-        this.setState({...this.state,currentPantry:activePantry} );
-
+        this.setState({ ...this.state, currentPantry: activePantry });
     }
-    render(){
-        return(
+    render() {
+        return (
             <Container fluid>
-    
+                <GoogleMap
+                    defaultOptions={{ styles: mapStyles }}
+                    zoom={18}
+                    center={{
+                        lat: this.state.defaultCenter.lat,
+                        lng: this.state.defaultCenter.lng,
+                    }}
+                >
+                    {this.state.markerList.map((locMarker) => (
+                        <Marker
+                            key={locMarker.id}
+                            position={{
+                                lat: locMarker.lat,
+                                lng: locMarker.lng,
+                            }}
+                            onClick={() => {
+                                console.log("MEOW");
 
-                   <GoogleMap
-                        zoom={18}
-                        center={{ lat: this.state.defaultCenter.lat, lng: this.state.defaultCenter.lng }}
-                        
-                    >  
-                        {
-                        this.state.markerList.map(locMarker => (
-                            <Marker
-                                key={locMarker.id}
-                                position={{lat: locMarker.lat,lng:locMarker.lng}}
-                                onClick={()=>{
-                                    console.log("MEOW");   
-                                
-                                    this.changeDefaultCenter(locMarker.lat,locMarker.lng);   
-                                    this.changeCurrentPantry(locMarker.pantryInfo);
-                                }
-                                }
-                            />      
-                        ))}
+                                this.changeDefaultCenter(
+                                    locMarker.lat,
+                                    locMarker.lng
+                                );
+                                this.changeCurrentPantry(locMarker.pantryInfo);
+                            }}
+                        />
+                    ))}
+                </GoogleMap>
 
-
-                        </GoogleMap>    
-
-                        <PantryInfo name={this.state.currentPantry.name}  name={this.state.currentPantry.name} address={this.state.currentPantry.address} 
-                        contactNo={this.state.currentPantry.contactNo}  gCash={this.state.currentPantry.gCash} gCashName={this.state.currentPantry.gCashName}
-                        faceBook={this.state.currentPantry.faceBook}/>
-
+                <PantryInfo
+                    name={this.state.currentPantry.name}
+                    name={this.state.currentPantry.name}
+                    address={this.state.currentPantry.address}
+                    contactNo={this.state.currentPantry.contactNo}
+                    gCash={this.state.currentPantry.gCash}
+                    gCashName={this.state.currentPantry.gCashName}
+                    faceBook={this.state.currentPantry.faceBook}
+                />
             </Container>
-
-        );      
+        );
     }
 }
 const WrappedMap = withScriptjs(withGoogleMap(MapApi));
@@ -106,4 +162,3 @@ function Map() {
     );
 }
 export default Map;
-
--- src/components/Mapstyles.js
+++ src/components/Mapstyles.js
@@ -1,198 +1,198 @@
 export default [
     {
-        "featureType": "water",
-        "stylers": [
+        featureType: "water",
+        stylers: [
             {
-                "color":"sea blue"
-            }
-        ]
+                color: "sea blue",
+            },
+        ],
     },
     {
-        "featureType": "administrative",
-        "elementType": "labels.text.stroke",
-        "stylers": [
+        featureType: "administrative",
+        elementType: "labels.text.stroke",
+        stylers: [
             {
-                "color": "#ffffff"
+                color: "#ffffff",
             },
             {
-                "weight": 6
-            }
-        ]
+                weight: 6,
+            },
+        ],
     },
     {
-        "featureType": "administrative",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: "administrative",
+        elementType: "labels.text.fill",
+        stylers: [
             {
-                "color": "#e85113"
-            }
-        ]
+                color: "#e85113",
+            },
+        ],
     },
     {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [
             {
-                "color": "#efe9e4"
+                color: "#efe9e4",
             },
             {
-                "lightness": -40
-            }
-        ]
+                lightness: -40,
+            },
+        ],
     },
     {
-        "featureType": "road.arterial",
-        "elementType": "geometry.stroke",
-        "stylers": [
+        featureType: "road.arterial",
+        elementType: "geometry.stroke",
+        stylers: [
             {
-                "color": "#efe9e4"
+                color: "#efe9e4",
             },
             {
-                "lightness": -20
-            }
-        ]
+                lightness: -20,
+            },
+        ],
     },
     {
-        "featureType": "road",
-        "elementType": "labels.text.stroke",
-        "stylers": [
+        featureType: "road",
+        elementType: "labels.text.stroke",
+        stylers: [
             {
-                "lightness": 100
-            }
-        ]
+                lightness: 100,
+            },
+        ],
     },
     {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
             {
-                "lightness": -100
-            }
-        ]
+                lightness: -100,
+            },
+        ],
     },
     {
-        "featureType": "road.highway",
-        "elementType": "labels.icon"
+        featureType: "road.highway",
+        elementType: "labels.icon",
     },
     {
-        "featureType": "landscape",
-        "elementType": "labels",
-        "stylers": [
+        featureType: "landscape",
+        elementType: "labels",
+        stylers: [
             {
-                "visibility": "off"
-            }
-        ]
+                visibility: "off",
+            },
+        ],
     },
     {
-        "featureType": "landscape",
-        "stylers": [
+        featureType: "landscape",
+        stylers: [
             {
-                "lightness": 20
+                lightness: 20,
             },
             {
-                "color": "#efe9e4"
-            }
-        ]
+                color: "#efe9e4",
+            },
+        ],
     },
     {
-        "featureType": "landscape.man_made",
-        "stylers": [
+        featureType: "landscape.man_made",
+        stylers: [
             {
-                "visibility": "off"
-            }
-        ]
+                visibility: "off",
+            },
+        ],
     },
     {
-        "featureType": "water",
-        "elementType": "labels.text.stroke",
-        "stylers": [
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [
             {
-                "lightness": 100
-            }
-        ]
+                lightness: 100,
+            },
+        ],
     },
     {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
             {
-                "lightness": -100
-            }
-        ]
+                lightness: -100,
+            },
+        ],
     },
     {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
             {
-                "hue": "#11ff00"
-            }
-        ]
+                hue: "#11ff00",
+            },
+        ],
     },
     {
-        "featureType": "poi",
-        "elementType": "labels.text.stroke",
-        "stylers": [
+        featureType: "poi",
+        elementType: "labels.text.stroke",
+        stylers: [
             {
-                "lightness": 100
-            }
-        ]
+                lightness: 100,
+            },
+        ],
     },
     {
-        "featureType": "poi",
-        "elementType": "labels.icon",
-        "stylers": [
+        featureType: "poi",
+        elementType: "labels.icon",
+        stylers: [
             {
-                "hue": "#4cff00"
+                hue: "#4cff00",
             },
             {
-                "saturation": 58
-            }
-        ]
+                saturation: 58,
+            },
+        ],
     },
     {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [
             {
-                "visibility": "on"
+                visibility: "off",
             },
             {
-                "color": "#f0e4d3"
-            }
-        ]
+                color: "#f0e4d3",
+            },
+        ],
     },
     {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [
+        featureType: "road.highway",
+        elementType: "geometry.fill",
+        stylers: [
             {
-                "color": "#efe9e4"
+                color: "#efe9e4",
             },
             {
-                "lightness": -25
-            }
-        ]
+                lightness: -25,
+            },
+        ],
     },
     {
-        "featureType": "road.arterial",
-        "elementType": "geometry.fill",
-        "stylers": [
+        featureType: "road.arterial",
+        elementType: "geometry.fill",
+        stylers: [
             {
-                "color": "#efe9e4"
+                color: "#efe9e4",
             },
             {
-                "lightness": -10
-            }
-        ]
+                lightness: -10,
+            },
+        ],
     },
     {
-        "featureType": "poi",
-        "elementType": "labels",
-        "stylers": [
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
             {
-                "visibility": "simplified"
-            }
-        ]
-    }
-]
+                visibility: "off",
+            },
+        ],
+    },
+];
